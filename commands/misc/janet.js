5088d63d694f75232947516e56419d812e327c23
Richard Itokazo <god182@gmail.com>
2021-05-28 02:42:00 -0300

    Command Janet Colors

--- commands/misc/janet.js
+++ commands/misc/janet.js
@@ -12,6 +12,7 @@ module.exports = {
         .setURL(`https://discord.com/oauth2/authorize?=&client_id=846768664572723250&scope=bot&permissions=8`)
         .setImage('https://static.onecms.io/wp-content/uploads/sites/6/2018/12/nup_183523_0087-2000.jpg')
         .setTimestamp()
+        .setColor('7646FF')
         .setFooter(`Solicitado por ${message.author.username}`)
 
         return message.channel.send(embed)
