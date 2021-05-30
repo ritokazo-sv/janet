24648b56ffdaef1f9fda3114b47f6685f3a44ac9
Richard Itokazo <god182@gmail.com>
2021-05-29 23:31:10 -0300

    Deploy Test

--- commands/api/lol/rank.js
+++ commands/api/lol/rank.js
@@ -12,19 +12,43 @@ module.exports = {
     description: "Exibe informações do elo do Player ",
     callback: (message, arguments, text, client) => {
 
-        axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${arguments}?api_key=${riot_token}`)
+        const param = arguments.toString().replace(',', '+')
+        const endpoint = `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${param}?api_key=${riot_token}`;
+        let embed = new Discord.MessageEmbed()
+
+        axios.get(endpoint)
         .then((res) => {
             const player = res.data
+           
+            message.channel.send(`Consultando endpoint ${endpoint}...`).then( (resultMessage) => {
+                resultMessage.edit(`Endpoint consultado `)
+            })
+
         })
         .catch((err) => { 
             if(err.response) {
-                message.channel.send(`
-                > A API da Riot notificou um erro **${err.response.statusText}** com o código **${err.response.status}** 
-                \n > Estamos trabalhando para resolver isso.`) 
+
+                embed.setTitle(`Encontrei um erro`)
+                embed.setAuthor(client.user.username, client.user.avatarURL())
+                embed.setImage('https://www.autostraddle.com/wp-content/uploads/2019/10/janet-feature.jpg')
+                embed.setColor('7646FF')
+                embed.setTimestamp()
+                embed.setFooter(`Solicitado por ${message.author.username}`)
+
+                let msg_error = `
+                    A API da Riot notificou um erro **${err.response.statusText}** com o código **${err.response.status}** 
+                    \n Estamos trabalhando para resolver isso.
+                `
+
+                if(err.response.status && err.response.status == 404) {
+                    msg_error = `Jogador **${param}** não foi encontrado na Rito Gomes :smiling_face_with_tear:.`
+                }
+
+                embed.setDescription(msg_error)
+
+                message.channel.send(embed) 
             }            
         })
-
-        console.log(player)
 
         return
 
