11aa940b8d769694c307274c4faf589776ce007d
Richard Itokazo <god182@gmail.com>
2021-05-31 03:33:04 -0300

    New Commands Pics

---
+++ commands/api/hotpick.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js')
+const fetch = require("node-fetch");
+
+
+module.exports = {
+    commands: ['hot', 'sendhot', 'hots'],
+    minArgs: 0,
+    maxArgs: 1,
+    ignore: true,
+    description: "Tê dá uma sex pic random, ou envie para um amigo usando !sendhot",
+    callback: async (message, arguments, text, client) => {
+
+        const reddit = ['CelebsBR', 'Celebs']
+        const randreddit = reddit[Math.floor(Math.random() * reddit.length)]
+        const end = `https://www.reddit.com/r/${randreddit}.json?limit=100&?sort=top&t=all`
+        const endpoint = fetch(end)
+
+        const user = message.mentions.users.first()
+        let dmuser = null 
+        if(user) {
+            dmuser = client.users.cache.get(user.id)
+        }
+
+        endpoint.then(res => res.json())
+        .then(json => 
+            json.data.children.filter(v => v.data.url.includes('png') || v.data.url.includes('jpg') || v.data.url.includes('jpeg') ||  v.data.url.includes('gif')) 
+        )
+        .then((urls) => {
+            console.log(urls.length)
+
+            const randomURL = urls[Math.floor(Math.random() * urls.length) + 1]
+            
+            if(randomURL.data.title) {
+            
+                // Envia mensagem Embed with image
+                embed = new Discord.MessageEmbed()
+                .setTitle(`${randomURL.data.title}`)
+                .setURL(`${randomURL.data.url}`)
+                .setImage(`${randomURL.data.url}`)
+                .setColor('7646FF')
+                .setTimestamp()
+                .setFooter(`Solicitado por ${message.author.username}`)
+
+                if(dmuser) {
+                    embed.setDescription(`${message.author.username} te enviou essa imagem`)
+                    embed.setFooter(`Enviado por ${message.author.username}`)
+                    message.channel.send(`> Imagem preparada e enviada para **${dmuser.username}**!`)
+                    dmuser.send(embed)
+                    return
+                }
+
+                return message.channel.send(embed)
+            }
+
+        })
+        .catch((err) => {
+            message.reply(`Houve um erro buscando seu meme ${err}`)
+        })
+
+    },
+  }
