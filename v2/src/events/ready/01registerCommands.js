bec034dd979200733b5d47e382243bda117a759d
Richard Itokazo <ritokazo@studiovisual.com.br>
2023-09-08 21:06:32 -0300

    Add multi server from registering commands

--- v2/src/events/ready/01registerCommands.js
+++ v2/src/events/ready/01registerCommands.js
@@ -8,43 +8,48 @@ module.exports = async (client) => {
         const localCommands = getLocalCommands();
         const applicationCommands = await getApplicationCommands(client, testServer);
 
-        for (const localCommand of localCommands) {
-            const { name, description, options } = localCommand;
+        console.log(applicationCommands);
 
-            const existingCommand = await applicationCommands.cache.find(
-                (cmd) => cmd.name === name
-            );
+        for (appCommand of applicationCommands) {
+            console.log(`Checking commands for "${appCommand.guild.name}"`);
 
-            if(existingCommand) {
-                if(localCommands.deleted) {
-                    await applicationCommands.delete(existingCommand.id);
-                    continue;
-                }
-
-                if(areCommandsDifferent(existingCommand, localCommand)) {
-                    await applicationCommands.edit(existingCommand.id, {
+            for (const localCommand of localCommands) {
+                const { name, description, options } = localCommand;
+    
+                const existingCommand = await appCommand.cache.find(
+                    (cmd) => cmd.name === name
+                );
+    
+                if(existingCommand) {
+                    if(localCommands.deleted) {
+                        await appCommand.delete(existingCommand.id);
+                        continue;
+                    }
+    
+                    if(areCommandsDifferent(existingCommand, localCommand)) {
+                        await appCommand.edit(existingCommand.id, {
+                            description,
+                            options
+                        });
+    
+                        console.log(`Edited command "${name}"`);
+                    }
+                } else {
+                    if(localCommand.deleted) {
+                        console.log(`Skipping registering command "${name}" as it's set to delete.`);
+                        continue;
+                    }
+    
+                    await appCommand.create({
+                        name,
                         description,
                         options
                     });
-
-                    console.log(`Edited command "${name}"`);
+    
+                    console.log(`Registered command "${name}"`);
                 }
-            } else {
-                if(localCommand.deleted) {
-                    console.log(`Skipping registering command "${name}" as it's set to delete.`);
-                    continue;
-                }
-
-                await applicationCommands.create({
-                    name,
-                    description,
-                    options
-                });
-
-                console.log(`Registered command "${name}"`);
             }
         }
-
     } catch (error) {
         console.error(`There was an error: ${error}`);
     }
