bae5c83a38cd24d1b521c512b4c01ad7b16ff8f2
Richard Itokazo <ritokazo@studiovisual.com.br>
2023-09-11 19:06:30 -0300

    Add multi server

--- v2/src/events/ready/01registerCommands.js
+++ v2/src/events/ready/01registerCommands.js
@@ -6,51 +6,41 @@ const getLocalCommands = require('../../utils/getLocalCommands');
 module.exports = async (client) => {
   try {
     const localCommands = getLocalCommands();
-    const applicationCommands = await getApplicationCommands(
-      client,
-      testServer
-    );
 
-    for (const localCommand of localCommands) {
-      const { name, description, options } = localCommand;
+    // Loop through all the guilds the bot is in
+    for (const guild of client.guilds.cache.values()) {
+      console.log(`Checking commands for guild: ${guild.name}`);
 
-      const existingCommand = await applicationCommands.cache.find(
-        (cmd) => cmd.name === name
-      );
+      const applicationCommands = await getApplicationCommands(client, guild.id);
 
-      if (existingCommand) {
-        if (localCommand.deleted) {
-          await applicationCommands.delete(existingCommand.id);
-          console.log(`🗑 Deleted command "${name}".`);
-          continue;
+      for (const localCommand of localCommands) {
+        const { name, description, options } = localCommand;
+
+        const existingCommand = applicationCommands.cache.find(cmd => cmd.name === name);
+
+        if (existingCommand) {
+          if (localCommand.deleted) {
+            await applicationCommands.delete(existingCommand.id);
+            console.log(`🗑 Deleted command "${name}" in guild "${guild.name}".`);
+            continue;
+          }
+
+          if (areCommandsDifferent(existingCommand, localCommand)) {
+            await applicationCommands.edit(existingCommand.id, { description, options });
+            console.log(`🔁 Edited command "${name}" in guild "${guild.name}".`);
+          }
+        } else {
+          if (localCommand.deleted) {
+            console.log(`⏩ Skipping registering command "${name}" in guild "${guild.name}" as it's set to delete.`);
+            continue;
+          }
+
+          await applicationCommands.create({ name, description, options });
+          console.log(`👍 Registered command "${name}" in guild "${guild.name}".`);
         }
-
-        if (areCommandsDifferent(existingCommand, localCommand)) {
-          await applicationCommands.edit(existingCommand.id, {
-            description,
-            options,
-          });
-
-          console.log(`🔁 Edited command "${name}".`);
-        }
-      } else {
-        if (localCommand.deleted) {
-          console.log(
-            `⏩ Skipping registering command "${name}" as it's set to delete.`
-          );
-          continue;
-        }
-
-        await applicationCommands.create({
-          name,
-          description,
-          options,
-        });
-
-        console.log(`👍 Registered command "${name}."`);
       }
     }
   } catch (error) {
-    console.log(`There was an error: ${error}`);
+      console.log(`There was an error: ${error}`);
   }
 };
